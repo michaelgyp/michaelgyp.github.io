beb97e49feeda19b548b3c8b942b94b0f096f862
miki0805 <michaeladventure080510@gmail.com>
2025-06-06 23:58:24 +0100

    `Refactored website layout and styles, updated links and social media platforms, and modified JavaScript functionality to match the changes.`

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const links = document.querySelectorAll('[data-target]');
-const pages = document.querySelectorAll('.page');
+function showPageFromHash() {
+    const pages = document.querySelectorAll('.page');
+    const hash = window.location.hash || '#home'; // default to #home
+    const targetId = hash.replace('#', '');
 
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        const targetId = link.getAttribute('data-target');
-
-        pages.forEach(page => {
-            page.classList.remove('active');
-        });
-
-        const targetPage = document.getElementById(targetId);
-        if (targetPage) {
-            targetPage.classList.add('active');
-        }
+    pages.forEach(page => {
+        page.classList.remove('active');
     });
-});
+
+    const targetPage = document.getElementById(targetId);
+    if (targetPage) {
+        targetPage.classList.add('active');
+    }
+}
+
+window.addEventListener('DOMContentLoaded', showPageFromHash);
+window.addEventListener('hashchange', showPageFromHash);
